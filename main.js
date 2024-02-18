22937973666087e8dd8cdb523397b5ff7b084e6f
scottpjsco <137368865+scottpjsco@users.noreply.github.com>
2024-02-18 19:24:29 +0900

    Added functionality to the number buttons, the operators and the clear button.

--- main.js
+++ main.js
@@ -1,4 +1,49 @@
+let operator = '';
+let previousValue = '';
+let currentValue = '';
+
+
 document.addEventListener("DOMContentLoaded", function(){
     //store all components of HTML in or JS
+    let clear = document.querySelector("#clear-btn");
+    let equal = document.querySelector(".equal");
+    let decimal = document.querySelector(".decimal");
+
+    let numbers = document.querySelectorAll(".number");
+    let operators = document.querySelectorAll(".operator");
+
+    let previousScreen = document.querySelector(".previous");
+    let currentScreen = document.querySelector(".current");
+
+    numbers.forEach((number) => number.addEventListener("click", function(e){
+        handleNumber(e.target.textContent)
+        currentScreen.textContent = currentValue;
+    }))
+
+    operators.forEach((op) => op.addEventListener("click", function (e){
+        handleOperator(e.target.textContent)
+        previousScreen.textContent = previousValue + " " + operator;
+        currentScreen.textContent = currentValue;
+    }))
+
+    clear.addEventListener("click", function(){
+        previousValue = '';
+        currentValue = '';
+        operator = '';
+        previousScreen.textContent = currentValue;
+        currentScreen.textContent = currentValue;
+    })
 
 })
+
+function handleNumber (num) {
+    if (currentValue.length <= 7){
+    currentValue += num;
+    }
+}
+
+function handleOperator(op){
+    operator = op;
+    previousValue = currentValue;
+    currentValue = '';
+}
